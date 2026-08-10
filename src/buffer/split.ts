fe890bd8208ba700ea7ba13bdbd4963c0c8d8718
ROBOT <tkata12@outlook.jp>
2026-08-10 23:13:29 +0900

    feat: refactor splitBuf function to accept chunk size parameter

--- src/buffer/split.ts
+++ src/buffer/split.ts
@@ -1,9 +1,13 @@
-export function split8(buf: Buffer): Buffer[] {
+export function splitBuf(buf: Buffer, num: number): Buffer[] {
   const chunks: Buffer[] = [];
 
-  for (let i = 0; i < buf.length; i += 8) {
-    chunks.push(buf.subarray(i, i + 7));
+  for (let i = 0; i < buf.length; i += num) {
+    chunks.push(buf.subarray(i, i + (num - 1)));
   }
 
   return chunks;
+}
+
+export function split8(buf: Buffer): Buffer[] {
+  return splitBuf(buf, 8);
 }
